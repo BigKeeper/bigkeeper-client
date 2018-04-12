646ce70a8fffe768685c9690614dd53ab9622855
mmoaay <mmoaay@sina.com>
2018-04-12 15:57:31 +0800

    update home

--- src/renderer/components/HomePage.vue
+++ src/renderer/components/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="wrapper">
-    <el-container style="height: 600px; border: 1px solid #eee">
+    <el-container style="border: 1px solid #eee">
       <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
         <el-container style="height: 160px; border: 1px solid #eee">
         </el-container>
